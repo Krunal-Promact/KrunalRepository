e8ac9356f53a45477d2e81175994c5a13f5402f3
krunal-promact <kunal@promactinfo.com>
2016-03-29 17:13:02 +0530

    Database Changes

---
+++ GitMvcWebApplication/GitMvcWebApplication/Models/KrunalContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace GitMvcWebApplication.Models
+{
+    public class KrunalContext : DbContext
+    {
+        public KrunalContext() : base("KrunalDbContext")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ GitMvcWebApplication/GitMvcWebApplication/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace GitMvcWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
